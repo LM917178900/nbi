6fc6f56f83889af184534729f7dbd31f4fe67433
leiming5 <leiming5@lenovo.com>
2022-03-07 11:12:16 +0800

    create tree Modules

---
+++ portal/src/test/java/com/only/nbi/NbiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.only.nbi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
