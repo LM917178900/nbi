4f0bd81161217f84128b7922a3afb548f3e56beb
leiming5 <leiming5@lenovo.com>
2022-03-21 12:31:07 +0800

    添加 添加数据库链接需要的注解和配置

--- portal/src/main/java/com/only/nbi/NbiApplication.java
+++ portal/src/main/java/com/only/nbi/NbiApplication.java
@@ -1,8 +1,10 @@
 package com.only.nbi;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan({"com.only.nbi.**.mapper"})
 @SpringBootApplication
 public class NbiApplication {
 
